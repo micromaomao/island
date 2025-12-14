e5534cc19f4e40544c8fd228d2f9f12dae258d51
Tingmao Wang <m@maowtm.org>
2025-12-14 17:09:50 +0000

    Initial content for hook.fish
    Signed-off-by: Tingmao Wang <m@maowtm.org>

---
+++ assets/shell/hook.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+# Island shell integration for Fish: https://github.com/landlock-lsm/island
+#
+# # Usage
+#
+# Add this to your ~/.config/fish/config.fish:
+#
+#   source (island hook fish | psub)
