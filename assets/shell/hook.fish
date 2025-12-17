0d5d842e7f151495c714790bff94e161be850316
Tingmao Wang <m@maowtm.org>
2025-12-17 23:12:37 +0000

    Document limitation regarding fish function buffering

--- assets/shell/hook.fish
+++ assets/shell/hook.fish
@@ -33,12 +33,36 @@
 # - Idempotency: This script is safe to source multiple times.
 #
 # # Limitations
+#
 # - No handling of command substitutions.
+#
 # - Since aliases are defined as functions in fish, they are not easy to
 #   resolve automatically.  For alias-like behavior, use `abbr` instead,
 #   which will automatically expand before execution.
+#
 # - No support for functions.  This includes things like the built-in `ls`
 #   and `ll`, `man`, etc.  (They will still work, but won't be sandboxed.)
+#
+# - fish functions does not support streaming output when inside a
+#   pipeline (all outputs are buffered and printed at once at exit) [1][2].
+#   This means that with this hook, a command like
+#
+#     base64 /dev/urandom | head
+#
+#   will show nothing and eventually run out of memory, since `base64`
+#   (and `head`) is redefined to be a function.  `nosandbox` does not help
+#   here, to work around this, you need to use `command` on either side:
+#
+#     command base64 /dev/urandom | head
+#     base64 /dev/urandom | command head
+#
+#   alternatively, to still sandbox both end of the pipe, you can manually
+#   start a one-off shell:
+#
+#     fish -c 'base64 /dev/urandom | head'
+#
+#  [1]: https://github.com/fish-shell/fish-shell/issues/1396
+#  [2]: https://github.com/fish-shell/fish-shell/issues/5635
 #
 # # Note
 # - Fish has no ${(z)BUFFER} equivalent (`commandline --tokens-expanded` /
